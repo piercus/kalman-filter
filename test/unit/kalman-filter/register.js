d90e04014ab40d59e35a2d32dad8502347cefb3c
Adrien Pellissier <adrienp@getklap.com>
2020-07-06 12:59:59 +0200

    add test constant position #9

--- test/unit/kalman-filter/register.js
+++ test/unit/kalman-filter/register.js
@@ -2,11 +2,32 @@ const test = require('ava');
 
 const KalmanFilter = require('../../../lib/kalman-filter.js');
 const State = require('./state.js');
-//Verify that we can use a registered model
+//Verify that we can use a registered model, the observations are here in 1D
 
 
 test('Check constant position', t => {
-
+	const previousCorrected = new State({
+		mean: [[0]],
+		covariance: [[1]]
+	});
+	const kf = new KalmanFilter({
+		observation: {
+			sensorDimension: 1,
+			covariance: [[1]],
+			stateProjection: [[1]]
+		},
+		dynamic: {
+			dimension: 1,
+			name: 'constant-position',
+			covariance: [[1]]
+		}
+	});
+	const observations = [[0.1],[0.2], [0.1]];
+	const res = kf.filter(observations[0]);
+	const stateObjective = new State({
+		mean: [[]]
+	})
+	t.true(equalState(res, stateObjective));
 });
 
 
@@ -20,13 +41,15 @@ test('Check sensor', t => {
 });
 
 
-//Verify that we can register a model
+//Verify that we can register a model and use it correctly
 
 test('Registering custom speed', t => {
 	const kf = new KalmanFilter({
-		dynamic: 'constant-speed',
-		timeStep: 0.1,
-		covariance: [1, 0.01]
+		dynamic: {
+			name: 'constant-speed',
+			timeStep: 0.1,
+			covariance: [1, 0.01]
+		}
 	});
 
 	registerDynamic('custom-speed', function(){
@@ -53,4 +76,7 @@ test('Registering custom speed', t => {
 	t.true(predicted1 instanceof State);
 	t.true(predicted2 instanceof State);
 	t.true(equalState(predicted1, predicted2));
+
+	// Verify that the model had been correctly added to the list of exiting models
+	t.true(registeredModels.some(model => model.name === 'custom-speed'));
 });
