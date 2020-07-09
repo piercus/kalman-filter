56c4ed690f06158675b82ee5b3103089a12622c9
Adrien Pellissier <adrienp@getklap.com>
2020-07-09 16:08:30 +0200

    Changes in functions distribution #9

---
+++ test/unit/kalman-filter/logger.js
@@ -0,0 +1,25 @@
+const test = require('ava');
+const KalmanFilter = require('../../../lib/kalman-filter');
+
+test('Logger.debug', t => {
+	let hasDebug = false;
+	const kf = new KalmanFilter({
+		observation: {
+			name: 'sensors'
+		},
+		dynamic: {
+			name: 'constant-position'
+		},
+		logger: {
+			info: (...args) => console.log(...args),
+			debug: (...args) => {
+				hasDebug = true;
+				console.log(...args);
+			},
+			warn: (...args) => console.log(...args),
+			error: (...args) => console.log(...args)
+		}
+	});
+	kf.predict();
+	t.is(hasDebug, true);
+});
--- test/unit/kalman-filter/register.js
+++ test/unit/kalman-filter/register.js
@@ -3,6 +3,8 @@ const test = require('ava');
 const KalmanFilter = require('../../../lib/kalman-filter.js');
 const State = require('../../../lib/state.js');
 const equalState = require('../../helpers/equal-state.js');
+const modelCollection = require('../../../lib/model-collection.js');
+const identity = require('../../../lib/linalgebra/identity.js');
 
 // Verify that we can use a registered model, the observations are here in 1D
 
@@ -22,15 +24,15 @@ test('Check constant position', t => {
 			name: 'constant-position'
 		}
 	});
-	const observations = [[0.1], [0.2], [0.1]];
-	const result = kf.predict();
-	// Const stateObjective = new State({
-	// 	mean: [[2 / 30]],
-	// 	covariance: [[2 / 3]]
-	// });
+	// Const observations = [[0.1], [0.2], [0.1]];
+	const result = kf.predict({previousCorrected});
+	const stateObjective = new State({
+		mean: [[0]],
+		covariance: [[2]]
+	});
 	t.true(result instanceof State);
-	// // We verify that the registered model returns the good correction
-	// t.true(equalState(result, stateObjective));
+	// We verify that the registered model returns the good correction
+	t.true(equalState(result, stateObjective));
 });
 
 test('Check constant speed', t => {
@@ -44,7 +46,8 @@ test('Check constant speed', t => {
 	const kf = new KalmanFilter({
 		observation: {
 			dimension: 1,
-			covariance: [[1]]
+			covariance: [[1]],
+			stateProjection: [[1, 0]]
 		},
 		dynamic: {
 			name: 'constant-speed',
@@ -56,19 +59,22 @@ test('Check constant speed', t => {
 		}
 	});
 	const observations = [[0.11], [0.21], [0.3]];
-	const result = kf.predict();
-	// Result calculted by hand
-	// const timeStep = 0.1;
-	// const stateObjective = new State({
-	// 	mean: [[timeStep + 0.006], [1]],
-	// 	covariance: [
-	// 		[0.66, 0.33 * timeStep],
-	// 		[0, 1.01]
-	// 	]
-	// });
-	t.true(result instanceof State);
+	const predicted = kf.predict({previousCorrected});
+	const corrected = kf.correct({predicted, observation: observations[0]});
+	// Result calculated by hand
+	const timeStep = 0.1;
+	const stateObjective = new State({
+		mean: [[timeStep + 0.006], [1]],
+		covariance: [
+			[0.66, 0.33 * timeStep],
+			[0, 1.01]
+		]
+	});
+	t.true(predicted instanceof State);
+	t.true(corrected instanceof State);
+	console.log('Corrected', corrected);
 	// We verify that the registered model returns the good result
-	// t.true(equalState(result, stateObjective));
+	t.true(equalState(corrected, stateObjective, 0.1));
 });
 
 test('Check sensor', t => {
@@ -111,7 +117,7 @@ test('Check sensor', t => {
 		}
 	});
 	const observations = [[[0.11], [0.1]], [[0.21], [0.19]], [[0.3], [0.3]]];
-	const result = kf.predict();
+	const result = kf.predict({previousCorrected, observation: observations[0]});
 	t.true(result instanceof State);
 
 	const stateObjective = kf2.predict({previousCorrected, observation: observations[0]});
@@ -126,19 +132,36 @@ test('Registering custom speed', t => {
 			name: 'constant-speed',
 			timeStep: 0.1,
 			covariance: [1, 0.01]
+		},
+		observation: {
+			name: 'sensors'
 		}
 	});
-	const timeStep = 0.1;
-	KalmanFilter.registerDynamic('custom-speed', () => {
-		const dimension = 2;
-		const transition = [
-			[1, timeStep],
-			[0, 1]
-		];
-		const covariance = [
-			[1, 0],
-			[0, 0.01]
-		];
+	modelCollection.registerDynamic('custom-speed', (dynamic, observation) => {
+		const timeStep = dynamic.timeStep || 1;
+		const observedProjection = observation.observedProjection;
+		const stateProjection = observation.stateProjection;
+		const observationDimension = observation.dimension;
+		let dimension;
+		if (stateProjection && Number.isInteger(stateProjection[0].length / 2)) {
+			dimension = observation.stateProjection[0].length;
+		} else if (observedProjection) {
+			dimension = observedProjection[0].length * 2;
+		} else if (observationDimension) {
+			dimension = observationDimension * 2;
+		} else {
+			throw (new Error('observedProjection or stateProjection should be defined in observation in order to use constant-speed filter'));
+		}
+
+		const baseDimension = dimension / 2;
+		// We construct the transition and covariance matrices
+		const transition = identity(dimension);
+		for (let i = 0; i < baseDimension; i++) {
+			transition[i][i + baseDimension] = timeStep;
+		}
+
+		const arrayCovariance = new Array(baseDimension).fill(1).concat(new Array(baseDimension).fill(timeStep * timeStep));
+		const covariance = dynamic.covariance || arrayCovariance;
 		return {dimension, transition, covariance};
 	});
 
@@ -147,6 +170,9 @@ test('Registering custom speed', t => {
 			name: 'custom-speed',
 			timeStep: 0.1,
 			covariance: [1, 0.01]
+		},
+		observation: {
+			name: 'sensors'
 		}
 	});
 	const predicted1 = kf.predict();
@@ -157,5 +183,5 @@ test('Registering custom speed', t => {
 	t.true(equalState(predicted1, predicted2));
 
 	// Verify that the model had been correctly added to the list of exiting models
-	t.true(KalmanFilter.registeredModels.some(model => model.name === 'custom-speed'));
+	//t.true(KalmanFilter.registeredModels.some(model => model.name === 'custom-speed'));
 });
