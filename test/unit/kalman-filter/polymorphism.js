d86f93bdd5fd4e9061b2d6ba95d0e183cfdd0154
Adrien Pellissier <adrienp@getklap.com>
2020-07-20 09:55:30 +0200

    predicted.index not NaN #19

--- test/unit/kalman-filter/polymorphism.js
+++ test/unit/kalman-filter/polymorphism.js
@@ -310,3 +310,25 @@ test('Observed and State Projections', t => {
 	});
 	t.is(error.message, 'You cannot use both observedProjection and stateProjection');
 });
+
+// Test index is not NaN if undefined
+
+test('Index initialization', t => {
+	const kf = new KalmanFilter(defaultOptions);
+	const firstState = new State({
+		mean: [[0], [0], [0], [0]],
+		covariance: [
+			[1, 0, 0, 0],
+			[0, 1, 0, 0],
+			[0, 0, 1, 0],
+			[0, 0, 0, 1]
+		]
+	});
+	const predicted1 = kf.predict();
+	const predicted2 = kf.predict({previousCorrected: firstState});
+	console.log('Index', Number.isNaN(predicted1.index))
+	t.false(Number.isNaN(predicted1.index));
+	t.false(Number.isNaN(predicted2.index));
+	t.deepEqual(predicted1.index, 'Index key is not defined');
+	t.deepEqual(predicted2.index, 'Index key is not defined');
+})
