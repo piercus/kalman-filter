8b7faa42a0eadf523db8917e0bd10ab6958afec1
Pierre Colle <piercus@gmail.com>
2020-10-07 19:00:11 +0200

    clean up xo

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ module.exports = {
 	registerDynamic: modelCollection.registerDynamic,
 	KalmanFilter: require('./lib/kalman-filter'),
 	registerObservation: modelCollection.registerObservation,
-	buildObservation:modelCollection.buildObservation,
+	buildObservation: modelCollection.buildObservation,
 	buildDynamic: modelCollection.buildDynamic,
 	getCovariance: require('./lib/utils/get-covariance'),
 	State: require('./lib/state')
