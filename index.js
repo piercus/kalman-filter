6a4b19a4905c4bc98e4dd864b47444a3ebca75ee
Pierre Colle <piercus@gmail.com>
2020-10-07 18:58:34 +0200

    fix: pass input through the extended filter functions

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ module.exports = {
 	registerDynamic: modelCollection.registerDynamic,
 	KalmanFilter: require('./lib/kalman-filter'),
 	registerObservation: modelCollection.registerObservation,
+	buildObservation:modelCollection.buildObservation,
+	buildDynamic: modelCollection.buildDynamic,
 	getCovariance: require('./lib/utils/get-covariance'),
 	State: require('./lib/state')
 };
