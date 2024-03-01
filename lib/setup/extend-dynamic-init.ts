2785470f3c881adafbd93ee7a067b2b775573346
urielch <uriel.chemouni@gmail.com>
2024-03-01 08:15:18 +0100

    update extendDynamicInit

--- lib/setup/extend-dynamic-init.ts
+++ lib/setup/extend-dynamic-init.ts
@@ -1,14 +1,17 @@
 import {diag} from 'simple-linalg';
 import polymorphMatrix from '../utils/polymorph-matrix';
+import {DynamicConfig, DynamicConfigParcial, ObservationConfig} from '../types/ObservationConfig';
 
 /**
  * Initializes the dynamic.init when not given
+ * Only used by setupModelsParameters
  * @param {ObservationConfig} observation
- * @param {DynamicConfig} dynamic
+ * @param {DynamicConfigParcial} dynamic
  * @returns {CoreConfig}
  */
 
-export default function extendDynamicInit({observation, dynamic}) {
+export default function extendDynamicInit(args: {observation: ObservationConfig, dynamic: DynamicConfigParcial}): {observation: ObservationConfig, dynamic: DynamicConfig} {
+	const {observation, dynamic} = args;
 	if (!dynamic.init) {
 		const huge = 1e6;
 		const dynamicDimension = dynamic.dimension;
@@ -16,13 +19,14 @@ export default function extendDynamicInit({observation, dynamic}) {
 		const covarianceArray = new Array(dynamicDimension).fill(huge);
 		const withInitOptions = {
 			observation,
-			dynamic: Object.assign({}, dynamic, {
+			dynamic: {
+				...dynamic,
 				init: {
 					mean: meanArray.map(element => [element]),
 					covariance: diag(covarianceArray),
 					index: -1,
 				},
-			}),
+			},
 		};
 		return withInitOptions;
 	}
@@ -31,9 +35,10 @@ export default function extendDynamicInit({observation, dynamic}) {
 		throw (new Error('dynamic.init should have a mean key'));
 	}
 
-	dynamic.init = Object.assign({}, dynamic.init, {
+	dynamic.init = {
+		...dynamic.init,
 		covariance: polymorphMatrix(dynamic.init.covariance, {dimension: dynamic.dimension}),
-	});
+	};
 
-	return {observation, dynamic};
+	return {observation, dynamic: dynamic as DynamicConfig};
 }
