2893d41bbd277b8f6c950cc29838bb5444852e2d
urielch <uriel.chemouni@gmail.com>
2024-03-01 08:16:33 +0100

    update buildStateProjection

--- lib/setup/build-state-projection.ts
+++ lib/setup/build-state-projection.ts
@@ -1,14 +1,16 @@
 import {padWithZeroCols as padWithZeros} from 'simple-linalg';
 import {identity} from 'simple-linalg';
+import {DynamicConfigParcial} from '../types/ObservationConfig';
 
 /**
-*Builds the stateProjection given an observedProjection
-*@param {ObservationConfig} observation
-*@param {DynamicConfig} dynamic
-*@returns {ObservationConfig, DynamicConfig} the model containing the created stateProjection
+* Builds the stateProjection given an observedProjection
+* Only used by setupModelsParameters
+* @param {ObservationConfig} observation
+* @param {DynamicConfig} dynamic
+* @returns {ObservationConfig, DynamicConfig} the model containing the created stateProjection
 */
-
-export default function buildStateProjection({observation, dynamic}) {
+export default function buildStateProjection(args: {observation, dynamic: DynamicConfigParcial}): {observation: any; dynamic: DynamicConfigParcial} {
+	const {observation, dynamic} = args;
 	const {observedProjection, stateProjection} = observation;
 	const observationDimension = observation.dimension;
 	const dynamicDimension = dynamic.dimension;
@@ -19,9 +21,10 @@ export default function buildStateProjection({observation, dynamic}) {
 	if (observedProjection) {
 		const stateProjection = padWithZeros(observedProjection, {columns: dynamicDimension});
 		return {
-			observation: Object.assign({}, observation, {
+			observation: {
+				...observation,
 				stateProjection,
-			}),
+			},
 			dynamic,
 		};
 	}
@@ -29,9 +32,10 @@ export default function buildStateProjection({observation, dynamic}) {
 	if (observationDimension && dynamicDimension && !stateProjection) {
 		const observationMatrix = identity(observationDimension);
 		return {
-			observation: Object.assign({}, observation, {
+			observation: {
+				...observation,
 				stateProjection: padWithZeros(observationMatrix, {columns: dynamicDimension}),
-			}),
+			},
 			dynamic,
 		};
 	}
